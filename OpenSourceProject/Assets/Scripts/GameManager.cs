ce879007bba5e9e0b0ff0824c38d39760e8a1f72
futureots <futureots113@gmail.com>
2025-05-08 16:13:25 +0900

    feat : 스테이지 점수 저장,로드 구현
    stagedata에 플레이한 stage점수와 시간 저장, Playerdata에서 스테이지별 데이터 저장

--- OpenSourceProject/Assets/Scripts/GameManager.cs
+++ OpenSourceProject/Assets/Scripts/GameManager.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-    // 적 종류
+    /// <summary>
+    /// 스테이지 단계
+    /// </summary>
+    public int stageNumber;
+    /// <summary>
+    /// 적 종류
+    /// </summary>
     public List<GameObject> enemyPrefabs;
 
-    // 현재 획득한 점수
+    /// <summary>
+    /// 현재 획득한 점수
+    /// </summary>
     public int point {  get; private set; }
     /// <summary>
     /// 점수를 추가하는 함수
@@ -17,20 +25,26 @@ public class GameManager : Singleton<GameManager>
     {
         this.point += point;
     }
-    // 스테이지에서 버틴 시간
+
+    /// <summary>
+    /// 스테이지에서 버틴 시간
+    /// </summary>
     public float time { get; private set; }
 
     private void Start()
     {
         point = 0;
         time = 0;
+        SpawnEnemy(5f);
+        SaveStageData();
     }
     private void Update()
     {
         time += Time.deltaTime;
 
     }
-    // 일정 시간마다 적을 스폰하는 코루틴
+
+    /// 일정 시간마다 적을 스폰하는 코루틴
     IEnumerator SpawnEnemy(float delay)
     {
         while (true)
@@ -38,9 +52,43 @@ public class GameManager : Singleton<GameManager>
             var randEnemy = enemyPrefabs[Random.Range(0, enemyPrefabs.Count)];
             var enemyInstance = Instantiate(randEnemy);
 
-            // enemyinstance dotween으로 위치 이동(적 내부 함수)
+            // enemyinstance DOTween으로 위치 이동
 
             yield return new WaitForSeconds(delay);
         }
     }
+    /// <summary>
+    /// 시간을 멈추는 함수(일시정지 옵션 또는 게임 종료 시 사용)
+    /// </summary>
+    /// <param name="isPause">true는 정지, false는 재생</param>
+    public static void PauseTime(bool isPause)
+    {
+        if (isPause)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+        
+    }
+
+    public void SaveStageData()
+    {
+        StageData stageData = new StageData(point, time);
+        var data = PlayerData.LoadPlayerData("Data");
+        if (data.data.ContainsKey(stageNumber))
+        {
+            data.data[stageNumber] = stageData;
+        }
+        else
+        {
+            data.data.Add(stageNumber, stageData);
+        }
+        data.SavePlayerData("Data");
+    }
+
+    
 }
+
---
+++ OpenSourceProject/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+
+public class PlayerData
+{
+    public Dictionary<int,StageData> data;
+
+    public PlayerData()
+    {
+        data = new Dictionary<int,StageData>();
+    }
+
+    #region ConvertJson
+
+    static JsonSerializerSettings serializerSettings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All };
+    public static string SerializePartyData(PlayerData data)
+    {
+        if (data == null) return null;
+        return JsonConvert.SerializeObject(data, serializerSettings);
+    }
+    public static PlayerData DeserializePartyData(string json)
+    {
+        if (json == null) return null;
+        return JsonConvert.DeserializeObject<PlayerData>(json, serializerSettings);
+    }
+
+    public void SavePlayerData(string fileName)
+    {
+        string data = SerializePartyData(this);
+        string path = Path.Combine(Application.dataPath + "/Data", fileName + ".Json");
+        File.WriteAllText(path, data);
+        Debug.Log(data);
+        Debug.Log("Save");
+    }
+    public static PlayerData LoadPlayerData(string fileName)
+    {
+        string path = Path.Combine(Application.dataPath + "/Data", fileName + ".Json");
+        string data = null;
+        if (File.Exists(path))
+        {
+            data = File.ReadAllText(path);
+        }
+        if (data == null)
+        {
+            return new PlayerData();
+        }
+        return DeserializePartyData(data);
+    }
+
+    #endregion
+}
+public struct StageData
+{
+    public StageData(int _point = 0, float _time = 0)
+    {
+        point = _point;
+        time = _time;
+    }
+    public int point;
+    public float time;
+}
